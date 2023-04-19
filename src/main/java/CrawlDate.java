f036165217533a9f79a5056eb553c9715f8b84fd
Minsoo Yeo <msyeo1404@gmail.com>
2023-04-19 13:09:29 +0900

    Added CrawlDate

---
+++ src/main/java/CrawlDate.java
@@ -0,0 +1,9 @@
+import java.time.LocalDateTime;
+
+public class CrawlDate {
+    private LocalDateTime lastCrawlDateTime;
+
+    public CrawlDate(LocalDateTime lastCrawlDateTime) {
+        this.lastCrawlDateTime = lastCrawlDateTime;
+    }
+}
