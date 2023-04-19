7bd644dd2c16bef86504b0cd7a2dae959c59e72f
Minsoo Yeo <msyeo1404@gmail.com>
2023-04-19 13:16:23 +0900

    Moved CrawlDate into Correct position

--- src/main/java/CrawlDate.java
+++
@@ -1,9 +0,0 @@
-import java.time.LocalDateTime;
-
-public class CrawlDate {
-    private LocalDateTime lastCrawlDateTime;
-
-    public CrawlDate(LocalDateTime lastCrawlDateTime) {
-        this.lastCrawlDateTime = lastCrawlDateTime;
-    }
-}
