149f84ffbbe12af2dec2fde4433b216c3bcc21ad
Minsoo Yeo <msyeo1404@gmail.com>
2023-04-20 13:18:27 +0900

    fixed times table of 2

--- src/main/java/com/likelion/javaproject2/week1/PrintfEx.java
+++ src/main/java/com/likelion/javaproject2/week1/PrintfEx.java
@@ -5,15 +5,15 @@ public class PrintfEx {
         System.out.printf("%s\n", "Hello");
         System.out.printf("%s * %s = %s\n", 1,1,1);
         PrintfEx.dateEx();
-        System.out.printf("%s * %s = %s\n", 2,1,2);
-        System.out.printf("%s * %s = %s\n", 2,2,4);
-        System.out.printf("%s * %s = %s\n", 2,3,6);
-        System.out.printf("%s * %s = %s\n", 2,4,8);
-        System.out.printf("%s * %s = %s\n", 2,5,10);
-        System.out.printf("%s * %s = %s\n", 2,6,12);
-        System.out.printf("%s * %s = %s\n", 2,7,14);
-        System.out.printf("%s * %s = %s\n", 2,8,16);
-        System.out.printf("%s * %s = %s\n", 2,9,18);
+        System.out.printf("%s * %s = %s\n", 2,1,2*1);
+        System.out.printf("%s * %s = %s\n", 2,2,2*2);
+        System.out.printf("%s * %s = %s\n", 2,3,2*3);
+        System.out.printf("%s * %s = %s\n", 2,4,2*4);
+        System.out.printf("%s * %s = %s\n", 2,5,2*5);
+        System.out.printf("%s * %s = %s\n", 2,6,2*6);
+        System.out.printf("%s * %s = %s\n", 2,7,2*7);
+        System.out.printf("%s * %s = %s\n", 2,8,2*8);
+        System.out.printf("%s * %s = %s\n", 2,9,2*9);
 
     }
 
