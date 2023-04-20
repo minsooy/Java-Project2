30e72116366b5828c83aea10eaf409c3b09d4ce8
Minsoo Yeo <msyeo1404@gmail.com>
2023-04-20 11:29:34 +0900

    Made PrintfEx to practice printf

---
+++ src/main/java/com/likelion/javaproject2/week1/PrintfEx.java
@@ -0,0 +1,9 @@
+package com.likelion.javaproject2.week1;
+
+public class PrintfEx {
+    public static void main(String[] args) {
+        System.out.printf("%s\n", "Hello");
+
+        System.out.printf("%s * %s = %s", 1,1,1);
+    }
+}
