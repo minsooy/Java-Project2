3ed9681eeb84fccfb587a58fc98b7df37c315f66
Minsoo Yeo <msyeo1404@gmail.com>
2023-04-21 13:22:12 +0900

    Added SpaceInvadersMemberVariable member variable practice

--- src/main/java/com/likelion/javaproject2/week1/day5/SpaceInvaderEx.java
+++ src/main/java/com/likelion/javaproject2/week1/day5/SpaceInvaderEx.java
@@ -4,10 +4,18 @@ public class SpaceInvaderEx {
     private void moveLeft() {
         int location = 0;
         location = -1;
+        System.out.printf("moveLeft: %d\n", location);
     }
     private void moveRight() {
         int location = 0;
-        location = 1;
+        location += 1;
+        System.out.printf("moveRight: %d\n", location);
+    }
+
+    public static void main(String[] args) {
+        SpaceInvaderEx si = new SpaceInvaderEx();
+        si.moveLeft();
+        si.moveRight();
     }
 
 }
