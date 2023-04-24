7e5f23762f4551ef973fac1577a51fe43f29f967
Minsoo Yeo <msyeo1404@gmail.com>
2023-04-24 15:35:01 +0900

    Added boolean practice

---
+++ src/main/java/com/likelion/javaproject2/week2/BooleanEx.java
@@ -0,0 +1,10 @@
+package com.likelion.javaproject2.week2;
+
+public class BooleanEx {
+    public static void main(String[] args) {
+        boolean flag1 = false;
+        boolean flag2 = true;
+
+        System.out.println(flag1 == flag2);
+    }
+}
---
+++ src/main/java/com/likelion/javaproject2/week2/IsAdult.java
@@ -0,0 +1,7 @@
+package com.likelion.javaproject2.week2;
+
+public class IsAdult {
+    public static void main(String[] args) {
+        int age = 27;
+    }
+}
