5c70d0a7b9075afffbc41c4dc772b036c9733327
Minsoo Yeo <msyeo1404@gmail.com>
2023-04-26 11:50:11 +0900

    fixed TwoDimArrayEx

--- src/main/java/com/likelion/javaproject2/week2/day3/TwoDimArrayEx.java
+++ src/main/java/com/likelion/javaproject2/week2/day3/TwoDimArrayEx.java
@@ -3,21 +3,25 @@ package com.likelion.javaproject2.week2.day3;
 import java.util.Arrays;
 
 public class TwoDimArrayEx {
+    public static void printArray(int[][] arr){
+        System.out.println(Arrays.toString(arr[0]));
+        System.out.println(Arrays.toString(arr[1]));
+        System.out.println(Arrays.toString(arr[2]));
+        System.out.println("-----------------------");
+
+
+    }
+
     public static void main(String[] args) {
         int[][] arr2 = new int[3][3];
 
-        System.out.println(Arrays.toString(arr2[0]));
-        System.out.println(Arrays.toString(arr2[1]));
-        System.out.println(Arrays.toString(arr2[2]));
-
-        System.out.println("-----------------------");
+        printArray(arr2);
 
         arr2[0][0] = 1;
         arr2[1][1] = 2;
         arr2[2][2] = 3;
 
-        System.out.println(Arrays.toString(arr2[0]));
-        System.out.println(Arrays.toString(arr2[1]));
-        System.out.println(Arrays.toString(arr2[2]));
+        printArray(arr2);
+
     }
 }
