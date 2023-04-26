0137734fc9804c9ef317c639e2c83b80b6a26541
Minsoo Yeo <msyeo1404@gmail.com>
2023-04-26 10:35:48 +0900

    added 2dimensional array practice

---
+++ src/main/java/com/likelion/javaproject2/week2/day3/ThreeByThree.java
@@ -0,0 +1,18 @@
+package com.likelion.javaproject2.week2.day3;
+
+public class ThreeByThree {
+    public static void main(String[] args) {
+        int[][] chart33 = new int[3][3];
+        chart33[0][0] = 1;
+        chart33[0][1] = 2;
+        chart33[0][2] = 3;
+        chart33[1][0] = 4;
+        chart33[1][1] = 5;
+        chart33[1][2] = 6;
+        chart33[2][0] = 7;
+        chart33[2][1] = 8;
+        chart33[2][2] = 9;
+
+
+    }
+}
