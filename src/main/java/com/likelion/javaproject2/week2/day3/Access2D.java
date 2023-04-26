03b185b2457afb002cd79e6b03e8ae7a86acf31e
Minsoo Yeo <msyeo1404@gmail.com>
2023-04-26 11:23:54 +0900

    practiced accessing 2d array

---
+++ src/main/java/com/likelion/javaproject2/week2/day3/Access2D.java
@@ -0,0 +1,17 @@
+package com.likelion.javaproject2.week2.day3;
+
+import java.util.Arrays;
+
+public class Access2D {
+    public static void main(String[] args) {
+        int[][] arr2 = {
+                {10, 20, 30},
+                {40, 50, 60},
+                {70, 80, 90}
+        };
+        System.out.println(arr2[0][1]);
+        System.out.println(arr2[1][1]);
+        System.out.println(arr2[2][0]);
+    }
+
+}
