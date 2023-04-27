3167b899585f4c9188e135401ba308fb615ef402
Minsoo Yeo <msyeo1404@gmail.com>
2023-04-27 11:43:39 +0900

    Added LogicTest and BoilerControl to practice logical operator

---
+++ src/main/java/com/likelion/javaproject2/week2/day4/BoilerControl.java
@@ -0,0 +1,10 @@
+package com.likelion.javaproject2.week2.day4;
+
+public class BoilerControl {
+    public static void main(String[] args) {
+        int roomTemp = 22;
+        int waterTemp = 45;
+
+        System.out.println(roomTemp <= 25 && waterTemp <= 50);
+    }
+}
---
+++ src/main/java/com/likelion/javaproject2/week2/day4/LogicTest.java
@@ -0,0 +1,10 @@
+package com.likelion.javaproject2.week2.day4;
+
+public class LogicTest {
+    public static void main(String[] args) {
+        System.out.println(true && true);
+        System.out.println(true && false);
+        System.out.println(false && true);
+        System.out.println(false && false);
+    }
+}
