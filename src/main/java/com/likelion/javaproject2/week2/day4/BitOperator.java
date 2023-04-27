ce7bc5c3fc98a15a2ae04a61dbf0d2b134f2a240
Minsoo Yeo <msyeo1404@gmail.com>
2023-04-27 16:03:10 +0900

    Added BitOperator to learn bit operations

---
+++ src/main/java/com/likelion/javaproject2/week2/day4/BitOperator.java
@@ -0,0 +1,13 @@
+package com.likelion.javaproject2.week2.day4;
+
+public class BitOperator {
+    public static void main(String[] args) {
+        int val1 = 1;
+        System.out.println(val1 << 1);
+        System.out.println(val1 << 2);
+
+        int val2 = 5;
+        System.out.println(val2 << 3);
+
+    }
+}
