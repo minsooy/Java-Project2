fa0e8dd981a5d130401544bf2adb9d1078e6b454
Minsoo Yeo <msyeo1404@gmail.com>
2023-04-28 09:26:16 +0900

    Practiced if statements with EvenOdd

---
+++ src/main/java/com/likelion/javaproject2/week2/day5/EvenOdd.java
@@ -0,0 +1,14 @@
+package com.likelion.javaproject2.week2.day5;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        int num = 7;
+
+        if (num % 2 == 0){
+            System.out.println("짝수 입니다.");
+        }
+        else{
+            System.out.println("홀수 입니다.");
+        }
+    }
+}
