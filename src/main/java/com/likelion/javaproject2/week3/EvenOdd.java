202919a36434bf638d869fbb9910dadbdec45b56
Minsoo Yeo <msyeo1404@gmail.com>
2023-05-01 13:14:57 +0900

    Added EvenOdd to print even and odd below 10

---
+++ src/main/java/com/likelion/javaproject2/week3/EvenOdd.java
@@ -0,0 +1,13 @@
+package com.likelion.javaproject2.week3;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i += 2) {
+            System.out.println(i);
+        }
+
+        for (int i = 1; i < 10; i += 2) {
+            System.out.println(i);
+        }
+    }
+}
