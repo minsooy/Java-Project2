89ac6a889a7fa778a1aff060f6f9c4990950c5f0
Minsoo Yeo <msyeo1404@gmail.com>
2023-05-01 14:35:59 +0900

    Added MultipleTimesTable for nested loop practice

---
+++ src/main/java/com/likelion/javaproject2/week3/MultipleTimesTable.java
@@ -0,0 +1,12 @@
+package com.likelion.javaproject2.week3;
+
+public class MultipleTimesTable {
+    public static void main(String[] args) {
+        for (int i = 2; i <= 4; i++) {
+            for (int j = 1; j <= 9 ; j++) {
+                System.out.printf("%d * %d = %d\n", i, j, i * j);
+            }
+            System.out.println("=============");
+        }
+    }
+}
