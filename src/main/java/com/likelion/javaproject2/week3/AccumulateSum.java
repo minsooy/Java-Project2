b2677e2a8aef269639322ebcbecfe8a34e437b94
Minsoo Yeo <msyeo1404@gmail.com>
2023-05-01 15:19:19 +0900

    Added AccumulateSum for adding 1~n

---
+++ src/main/java/com/likelion/javaproject2/week3/AccumulateSum.java
@@ -0,0 +1,12 @@
+package com.likelion.javaproject2.week3;
+
+public class AccumulateSum {
+    public static void main(String[] args) {
+        int answer  = 0;
+        for (int i = 1; i <= 10 ; i++) {
+            answer += i;
+        }
+
+        System.out.println(answer);
+    }
+}
