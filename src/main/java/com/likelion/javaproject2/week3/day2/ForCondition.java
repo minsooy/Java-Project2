570d543a3ae493ff073e0074cd632d560697d292
Minsoo Yeo <msyeo1404@gmail.com>
2023-05-02 10:47:03 +0900

    Added ForCondition to practice manipulating for conditions

---
+++ src/main/java/com/likelion/javaproject2/week3/day2/ForCondition.java
@@ -0,0 +1,17 @@
+package com.likelion.javaproject2.week3.day2;
+
+public class ForCondition {
+    public static void main(String[] args) {
+//        for (int i = 0; 1 == 1 ; i++) {
+//            System.out.println(i);
+//        }
+
+        for (int i = 0; i * 2 < 17 ; i++) {
+            System.out.println(i);
+        }
+
+        for (int i = 0; i * i < 50 ; i++) {
+            System.out.println(i);
+        }
+    }
+}
