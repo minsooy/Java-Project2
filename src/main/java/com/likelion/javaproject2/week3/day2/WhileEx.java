466aa699af902174b4d85c2c5a72e0bd76158a02
Minsoo Yeo <msyeo1404@gmail.com>
2023-05-02 14:22:05 +0900

    Added WhileEx to practice while loops

---
+++ src/main/java/com/likelion/javaproject2/week3/day2/WhileEx.java
@@ -0,0 +1,11 @@
+package com.likelion.javaproject2.week3.day2;
+
+public class WhileEx {
+    public static void main(String[] args) {
+        int count = 0;
+        while(count < 10){
+            System.out.printf("%d ",count);
+            count++;
+        }
+    }
+}
